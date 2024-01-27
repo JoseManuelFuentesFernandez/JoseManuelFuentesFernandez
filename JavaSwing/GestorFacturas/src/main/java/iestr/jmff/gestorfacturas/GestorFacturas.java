aacbe4fac89c9ddc0d98f19f4c3592d0631510b9
JMFF8 <fuentesfernandezjosemanuel@gmail.com>
2024-01-27 14:28:31 +0100

    Upload GestorFacturas

---
+++ JavaSwing/GestorFacturas/src/main/java/iestr/jmff/gestorfacturas/GestorFacturas.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package iestr.jmff.gestorfacturas;
+
+import iestr.jmff.gestorfacturas.vista.LogIn;
+
+/**
+ *
+ * @author Usuario
+ */
+public class GestorFacturas {
+
+    public static void main(String[] args) {
+        LogIn lg = new LogIn();
+        lg.show(true);
+    }
+}
